c7b035e7ae54f1f1fd0fcbb457916859f5ef394b
Zach Dodson <zdodson21@gmail.com>
2024-12-12 14:51:05 -0500

    TODO

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+# TODO Process to get this to work
+  # * imports for files and being able to read the files
+  # * read the files
+  # * find where the CSS is in the files
+  # * go through each CSS property to discover if it needs to be changed to utilize DDD
+  # * Print the results in the terminal with the line number, what property needs to be changed, and recommendations to change it to
+    # * Ensure anything that cannot be done with DDD is ignored.
