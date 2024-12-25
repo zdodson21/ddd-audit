b2205053ba90dda0726e35fa05f6f6c1f8d1d61b
Zach Dodson <zdodson21@gmail.com>
2024-12-24 22:47:42 -0500

    main function and minor fixes, plus dependency

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from styles import *
+from prettytable import PrettyTable
 
-# ! Developer Mode
 class colors:
     RED='\033[91m'
     GREEN='\033[92m'
@@ -13,37 +13,40 @@ class colors:
     GRAY='\033[90m'
     RESET='\033[0m'
 
+# ! Developer Mode
 developerModeEnabled = True #TODO Change to false when done
 
 def developer_mode(message):
     if developerModeEnabled:
         print(f"{colors.YELLOW}Developer Mode: {colors.GRAY}{message}")
 
-developer_mode("Activated")
-
-# ! Main code
+# ! Variables
 pwd = os.getcwd()
-developer_mode(f"Script called from: {colors.BLUE}{pwd}")
-
-# Get list of files to ignore
 ddd_ignore = []
-
-# Gets .dddignore if exist
-try:
-    with open(".dddignore", "r") as file:
-        for line in file:
-            if not line.startswith("#") and line.strip():
-                ddd_ignore.append(line.strip("/\n"))
-            else:
-                pass
-except FileNotFoundError:
-    pass
-
-developer_mode(f"Ignored files: {ddd_ignore}")
-
-# Get list of files to audit
 component_files = []
+ignore_exists = True
+audit_output = []
+
+def get_dddignore():
+    """
+    Gets values from .dddignore if it exists
+    """
+    try:
+        with open(".dddignore", "r") as file:
+            for line in file:
+                if not line.startswith("#") and line.strip():
+                    ddd_ignore.append(line.strip("/\n"))
+                else:
+                    pass
+        return True
+    except FileNotFoundError as error:
+        print(f"{colors.RED}{error}")
+        return False
+
 def get_files(directory):
+    """
+    Gets files to audit, ignoring files in .dddignore
+    """
     developer_mode(f"{colors.MAGENTA}Getting files from {directory}")
     
     for file in os.listdir(directory):
@@ -57,16 +60,43 @@ def get_files(directory):
         else:
             developer_mode(f"{colors.BLUE}Skipping {file}")
 
-get_files(pwd)
-
-developer_mode(f"Files to audit: {component_files}")
-
 # Start auditing
-for file in component_files:
-    developer_mode(f"{colors.GREEN}Auditing {file}")
+def audit_files():
+    """
+    Audits files, outputs to terminal the line number, what property needs to be changed, and recommendations to change it to
+    """
+    for file in component_files:
+        developer_mode(f"{colors.GREEN}Auditing {file}")
     # TODO Audit file
 
 # TODO find where the CSS is in the files
 # TODO go through each CSS property to discover if it needs to be changed to utilize DDD
-# TODO Print the results in the terminal with the line number, what property needs to be changed, and recommendations to change it to
-  # TODO Ensure anything that cannot be done with DDD is ignored.
+
+def output_table(auto_audit: bool):
+    """
+    Outputs a table of audited data, color coded to tell the user what can automatically be changed
+    """
+    # TODO REMOVE THIS LINK: https://stackoverflow.com/questions/9535954/printing-lists-as-tabular-data
+    table = PrettyTable(['Line Number', 'Property', 'Recommended Change'])
+    # TODO for loop add values, branch based on auto_audit value, color code everything. 
+
+
+if __name__ == "__main__":
+    developer_mode("Activated")
+
+    developer_mode(f"Script called from: {colors.BLUE}{pwd}")
+
+    # Gets .dddignore
+    if(get_dddignore()):
+        developer_mode("Found .dddignore")
+    else:
+        developer_mode("Unable to find .dddignore")
+        ignore_exists = False
+    developer_mode(f"Ignored files: {ddd_ignore}")
+
+    # Gets files to audit
+    get_files(pwd)
+    developer_mode(f"Files to audit: {component_files}")
+
+    # Audits files
+    audit_files()
